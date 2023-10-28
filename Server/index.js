fc76484f5ab67ac6efd2594a157f1eb1ea9815c0
wajihjabeur <jabeur.wajih@gmail.com>
2023-10-28 18:27:05 +0100

    server setup

---
+++ Server/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("connected to db"))
+  .catch((e) => console.log(e));
+app.listen(8000, () => "server is ready");
+
+
